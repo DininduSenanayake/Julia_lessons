b43f5ede8060d88a9fcbcc43107f900fe814a087
Dinindu Senanayake <dinindusen@gmail.com>
2022-01-11 20:32:14 +1300

    populate multithreading example

--- Intro/multithreadingNotes.jl
+++ Intro/multithreadingNotes.jl
@@ -1,3 +1,7 @@
+# notes are from http://www.cs.unb.ca/~aubanel/JuliaMultithreadingNotes.html
+
+#The Julia function returns an 8 bit integer, given the limited range of values.
+
 function juliaSetPixel(z0, c)
     z = z0
     niter = 255
@@ -8,11 +12,21 @@ function juliaSetPixel(z0, c)
     return niter%UInt8
 end
 
+#Calculation of one column of the image is computed with the following function:
 function calcColumn!(pic, c, n, j)
     x = -2.0 + (j-1)*4.0/(n-1)
     for i in 1:n
         y = -2.0 + (i-1)*4.0/(n-1)
         @inbounds pic[i,j] = juliaSetPixel(x+im*y, c)
+    end
+    nothing
+end
+
+#where it can be seen that the image has n x n pixels and the range of the coordinates is from -2 to 2. Calculation of the entire fractal is given by:
+
+function juliaSetCalc!(pic,c,n)
+    for j in 1:n
+        calcColumn!(pic,c,n,j)
     end
     nothing
 end
