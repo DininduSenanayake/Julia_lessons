19b69941a69ad8dbecedc9a3ba9d628c72277361
Dinindu Senanayake <dinindusen@gmail.com>
2022-01-28 20:10:57 +1300

    tutorial1 add a description

---
+++ ML/tutorial1.jl
@@ -0,0 +1,4 @@
+#ASo, the goal of this tutorial is to build a simple 
+#classification Neural Network. This will be enough for 
+#anyone who is interested in using Flux. After learning the very basics, 
+#the rest is pretty much altering Networks architectures and loss functions.
