433b41fc98708794895d981df8d99166a0be7919
Dinindu Senanayake <dinindusen@gmail.com>
2022-01-29 18:54:52 +1300

    prepare data 2

--- ML/irisdata.jl
+++ ML/irisdata.jl
@@ -5,3 +5,8 @@ using CSV, DataFrames
 iris = DataFrame(CSV.File("mypath//iris.csv"))
 
 #prepare data
+import Pkg; Pkg.add("Lathe")
+using Lathe
+scaled_feature = Lathe.preprocess.OneHotEncode(iris,:variety)
+iris = select!(iris, Not([:variety]))
+first(iris,5)
