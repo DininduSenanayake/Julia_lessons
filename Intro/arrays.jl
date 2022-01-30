f6a4e8d5c88433631661e052f481cbe6f75b3721
Dinindu Senanayake <dinindusen@gmail.com>
2022-01-30 17:50:00 +1300

    uninitialized arrays

--- Intro/arrays.jl
+++ Intro/arrays.jl
@@ -1,1 +1,7 @@
 #creating simple 2D arrays
+arr = [1,2,3]
+
+#Uninitialized array - Specify the type and dimension of an array by using the syntax Array{type}(dims)
+
+array = Array{Int64}(undef,3)
+
