9efb5eca3b77380270a071737daac652015c9a8d
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-01 17:58:49 +1300

    conv_mnist.jl package list

--- GPU/conv_mnist.jl
+++ GPU/conv_mnist.jl
@@ -2,3 +2,16 @@
 ## with the convolutional neural network known as LeNet5.
 ## This script also combines various
 ## packages from the Julia ecosystem with Flux.
+
+using Flux
+using Flux.Data: DataLoader
+using Flux.Optimise: Optimiser, WeightDecay
+using Flux: onehotbatch, onecold
+using Flux.Losses: logitcrossentropy
+using Statistics, Random
+using Logging: with_logger
+using TensorBoardLogger: TBLogger, tb_overwrite, set_step!, set_step_increment!
+using ProgressMeter: @showprogress
+import MLDatasets
+import BSON
+using CUDA
