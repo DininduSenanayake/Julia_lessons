4d89b03d014543a59152ed58ff08a7a659f1edc0
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-03 17:40:45 +1300

    add step 3

--- ML/FluxDataLoader.jl
+++ ML/FluxDataLoader.jl
@@ -16,3 +16,11 @@ test_x = reshape(test_x, 28, 28, 1, :)
 
 train_y, test_y = onehotbatch(train_y, 0:9), onehotbatch(test_y, 0:9)
 data_loader = DataLoader(train_x, train_y, batchsize=128, shuffle=true)
+
+#step 3 . iterating over the data
+
+for (x, y) in data_loader
+   @assert size(x) == (28, 28, 1, 128) || size(x) == (28, 28, 1, 96)
+   @assert size(y) == (10, 128) || size(y) == (10, 96)
+   ...
+end
