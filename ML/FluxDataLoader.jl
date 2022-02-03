b04435e0e8350619dc7913d42f02e4392a79caa8
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-03 17:40:17 +1300

    add more loading data

--- ML/FluxDataLoader.jl
+++ ML/FluxDataLoader.jl
@@ -15,3 +15,4 @@ train_x = reshape(train_x, 28, 28, 1, :)
 test_x = reshape(test_x, 28, 28, 1, :)
 
 train_y, test_y = onehotbatch(train_y, 0:9), onehotbatch(test_y, 0:9)
+data_loader = DataLoader(train_x, train_y, batchsize=128, shuffle=true)
