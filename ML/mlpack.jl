5d39be7ea9f45eed210f638081ab135b9f9baaa5
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-07 17:59:52 +1300

    popuplate mlpack.jl

--- ML/mlpack.jl
+++ ML/mlpack.jl
@@ -1,3 +1,8 @@
 #this is a simple example of how to use the mlpack Principal Component Analysis method on a randon dataset
 
 using mlpack
+
+x = rand(5,5);
+print("Input data:\n",x)
+
+print("\nPerforming PCS on dataset.")
