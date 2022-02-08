48e6317e3f435003fda53f4ab3c35c7c3031efd4
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-08 20:50:23 +1300

    split the labels

--- ML/mlpackQuickStart.jl
+++ ML/mlpackQuickStart.jl
@@ -7,3 +7,15 @@ using mlpack
 # want to use on the full dataset.
 df = CSV.read(ZlibInflateInputStream(open(download(
         "http://www.mlpack.org/datasets/covertype-small.csv.gz"))))
+
+
+# Split the labels.
+labels = df[!, :label][:]
+dataset = select!(df, Not(:label))
+
+# Split the dataset using mlpack.  The output comes back as a dictionary,
+# which we'll unpack for clarity of code.
+test, test_labels, train, train_labels = mlpack.preprocess_split(
+    input=dataset,
+    input_labels=labels,
+    test_ratio=0.3)
