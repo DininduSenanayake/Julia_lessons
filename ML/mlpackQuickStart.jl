c7fd15791c186f04fc11f996c66235578d2d2721
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-08 20:49:56 +1300

    load the dataet

--- ML/mlpackQuickStart.jl
+++ ML/mlpackQuickStart.jl
@@ -2,3 +2,8 @@ using CSV
 using DataFrames
 using Libz
 using mlpack
+
+# Load the dataset from an online URL.  Replace with 'covertype.csv.gz' if you
+# want to use on the full dataset.
+df = CSV.read(ZlibInflateInputStream(open(download(
+        "http://www.mlpack.org/datasets/covertype-small.csv.gz"))))
