39079d2e5eef5d8671c0a0441fa988152fde947c
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-09 20:37:16 +1300

    Load Packages

--- ML/simplemultilayer.jl
+++ ML/simplemultilayer.jl
@@ -1,3 +1,14 @@
 # extraced from https://fluxml.ai/tutorials/2021/01/26/mlp.html
 
 #Load packages
+using Flux, Statistics
+using Flux.Data: DataLoader
+using Flux: onehotbatch, onecold, logitcrossentropy, throttle, @epochs
+using Base.Iterators: repeated
+using Parameters: @with_kw
+using CUDA
+using MLDatasets
+if has_cuda()		# Check if CUDA is available
+    @info "CUDA is on"
+    CUDA.allowscalar(false)
+end
