cc348b591a6de17c030eb055eb125f6826737c07
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-16 20:51:37 +1300

    generate test date Int64

--- PerformanceBenchMark/firstless.jl
+++ PerformanceBenchMark/firstless.jl
@@ -7,3 +7,7 @@ BenchmarkTools.DEFAULT_PARAMETERS.seconds = 1000
 BenchmarkTools.DEFAULT_PARAMETERS.samples = 5
 
 # generate test data 
+endata(x, T) = [rand(typemin(T):typemax(T)) for y in 1:x]
+gendata (generic function with 1 method)
+
+int64_10x6 = gendata(10^6, Int64);
