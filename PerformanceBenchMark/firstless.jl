1b5cd9dc243a69e4aff325c28ab8283f7bdf9d49
Dinindu Senanayake <dinindusen@gmail.com>
2022-02-16 20:52:38 +1300

    peallocate

--- PerformanceBenchMark/firstless.jl
+++ PerformanceBenchMark/firstless.jl
@@ -11,3 +11,17 @@ endata(x, T) = [rand(typemin(T):typemax(T)) for y in 1:x]
 gendata (generic function with 1 method)
 
 int64_10x6 = gendata(10^6, Int64);
+
+#Test whether broadcasting more/less efficient than pre-allocating results array
+function preallocate(x)
+
+    v = Vector{OmniSci.TStringValue}(undef, length(x))
+
+    for idx in 1:length(x)
+        v[idx] = OmniSci.TStringValue(x[idx])
+    end
+
+    return v
+end
+
+preallocate (generic function with 1 method)
